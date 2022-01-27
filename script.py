e25b912ce4faf38267357a91dd33710ec001a4dd
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2022-01-27 09:47:57 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,16 +48,16 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @UvinduBro</b>
+<b>© @teamfoxbotz</b>
 """
 
 
     ABOUT_MSG = """⭕️<b>My Name : Filter Bot</b>
 
-⭕️<b>😴 Creater :</b> @UvinduBro  
+⭕️<b>😴 Creater :</b> @am_Dq_fan  
 
 ⭕️<b>🧷 Language :</b> <code>Python3</code>
 
-⭕️<b>🔥 Bot Updates :</b> <a href='https://t.me/UvinduBro_BOTs'> Uvindu Bro BOTs </a> 
+⭕️<b>🔥 Bot Updates :</b> <a href='https://t.me/teamfoxbotz'> സിനിമ കമ്പനി [DB] bot</a> 
 
 """
